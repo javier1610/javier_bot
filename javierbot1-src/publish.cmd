1399fad651283376fd62fd105fb8f28d9b4a8db3
Javier Gaspar <javiergaspar89@gmail.com>
2018-02-19 17:02:11 -0500

    commit_FirstBot

---
+++ javierbot1-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=javierbot1-Web-Deploy.pubxml -p:Password=KxCdftKylsaodpDv8dhCa3hFx5AW7b56CzG3LicG6urQj2b3ghmzkkk8poPa
+
